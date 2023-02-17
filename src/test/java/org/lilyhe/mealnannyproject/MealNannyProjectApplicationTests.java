a24f890e7ea24519abcec184f12de057b8361d8a
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2023-02-17 00:08:05 -0800

    Initial commit, multi-module set up

---
+++ src/test/java/org/lilyhe/mealnannyproject/MealNannyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lilyhe.mealnannyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealNannyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
