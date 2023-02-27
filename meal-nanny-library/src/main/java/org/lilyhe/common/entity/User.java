ae47a7f0b1a5f34c0dd28b06af82539aac774758
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2023-02-27 00:34:00 -0800

    Set up user class entity and getter setters methods. Figuring out how to map to database

---
+++ meal-nanny-library/src/main/java/org/lilyhe/common/entity/User.java
@@ -0,0 +1,114 @@
+package org.lilyhe.common.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// indicate this java class with map to a table in the database
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 80, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 64, nullable = false)
+    private String password;
+
+
+    @Column(name = "first_name", length = 40, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 40, nullable = false)
+    private String lastName;
+
+    private boolean active;
+
+
+    @ManyToMany
+    // unidirectional from user to role
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+            )
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+        // empty constructor
+    }
+
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public void addRole(Role role){
+        this.roles.add(role);
+    }
+}
