85043e490c86145595c7920d3318ac46caab5378
MadamHippo <88348716+MadamHippo@users.noreply.github.com>
2023-02-26 23:21:11 -0800

    Created assistant admin role and tested connection to database

--- meal-nanny-web/meal-nanny-backend/src/test/java/org/lilyhe/admin/user/RoleRepoTests.java
+++ meal-nanny-web/meal-nanny-backend/src/test/java/org/lilyhe/admin/user/RoleRepoTests.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 // using this annotation to override default spring JPA database to run test against a real db
 @AutoConfigureTestDatabase(replace = Replace.NONE)
+// tells Spring Boot not to undo or reverse the changes made to the database during a test.
+@Rollback(false)
 public class RoleRepoTests {
 
     // making a reference to the repository using autowired
@@ -25,5 +30,13 @@ public class RoleRepoTests {
         Role savedAdmin = repo.save(testAdmin);
         // making sure the Row object is actually in the database using assertThat API
         assertThat(savedAdmin.getId()).isGreaterThan(0);
+    }
+
+
+    @Test
+    public void testCreateAssistantRoles(){
+        Role roleAssistant = new Role("Assistant", "Helping out at the store, I ship, I sell, and I clean up well.");
+        // using list in case I want more administrative roles later
+        repo.saveAll(List.of(roleAssistant));
     }
 }
